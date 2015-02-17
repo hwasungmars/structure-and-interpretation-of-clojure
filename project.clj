231c819b00db42b56cd3af88fd4bc8170dec09c0
Hwasung Lee <hwasung.lee@oriel.oxon.org>
2015-02-17 21:49:05 +0000

    First commit with templates

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject structure-and-interpretation-of-clojure "0.1.0-SNAPSHOT"
+  :description "A Clojure translation of the book Structure and Interpreation of Computer Programs"
+  :url "http://technicallycorrect.info"
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.6.0"]])
---
+++ test/sic/core_test.clj
@@ -0,0 +1,7 @@
+(ns sic.core-test
+  (:require [clojure.test :refer :all]
+            [sic.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
