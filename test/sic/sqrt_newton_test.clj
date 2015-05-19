280a218f7a54f65532836dbe2c4179f0a126e3c1
Hwasung Lee <hwasung.lee@oriel.oxon.org>
2015-05-19 18:37:13 +0100

    Adding sqrt test.

--- test/sic/sqrt_newton_test.clj
+++ test/sic/sqrt_newton_test.clj
@@ -2,6 +2,6 @@
   (:require [clojure.test :refer :all]
             [sic.sqrt-newton :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 0))))
+(deftest sqrt-test
+  (testing "Square root of 4 should be 2."
+    (is (= 2.0 (sqrt 4)))))
